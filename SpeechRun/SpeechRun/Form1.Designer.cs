1ff58bef96ae11a3555bec8eda8db7eefd935972
Alexandr <all1992ex@gmail.com>
2018-03-04 16:17:46 +0400

    Create project.

---
+++ SpeechRun/SpeechRun/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SpeechRuntime
+{
+    partial class launchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.resultText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExit.ForeColor = System.Drawing.Color.IndianRed;
+            this.buttonExit.Location = new System.Drawing.Point(288, 180);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(102, 31);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelInfo.Location = new System.Drawing.Point(13, 13);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(59, 17);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Result:";
+            // 
+            // resultText
+            // 
+            this.resultText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultText.Location = new System.Drawing.Point(16, 33);
+            this.resultText.Name = "resultText";
+            this.resultText.Size = new System.Drawing.Size(374, 141);
+            this.resultText.TabIndex = 2;
+            this.resultText.Text = "";
+            // 
+            // launchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 223);
+            this.Controls.Add(this.resultText);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonExit);
+            this.Name = "launchForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher";
+            this.Load += new System.EventHandler(this.launchForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.RichTextBox resultText;
+    }
+}
+
---
+++ SpeechRun/SpeechRun/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+
+namespace SpeechRuntime
+{
+    public partial class launchForm : Form
+    {
+        static RichTextBox res;
+
+        public launchForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void launchForm_Load(object sender, EventArgs e)
+        {
+            res = resultText;
+
+            try
+            {
+                System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ru-ru");
+                SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+                sre.SetInputToDefaultAudioDevice();
+
+                sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Accepted);
+
+
+                Choices numbers = new Choices();
+                numbers.Add(new string[] { "выйти", "найди", "записать", "стоп", "помоги" });
+
+
+                GrammarBuilder gb = new GrammarBuilder();
+                gb.Culture = ci;
+                gb.Append(numbers);
+
+
+                Grammar g = new Grammar(gb);
+                sre.LoadGrammar(g);
+                gb.Culture = sre.RecognizerInfo.Culture;
+
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex) + 
+                    "\n\n   Программа будет работать не корректно, пока не будет исправлена ошибка!", "Error!");
+            }
+        }
+
+        static void Accepted(object sender, SpeechRecognizedEventArgs e)
+        {
+            try
+            {
+                Commands cmd = new Commands();
+
+                if (e.Result.Confidence > 0.87)
+                {
+                    res.Text = e.Result.Text;
+
+                    switch (res.Text)
+                    {
+                        case "выйти":
+                            Application.Exit();
+                            break;
+                        case "записать":
+                            cmd.WriteTxt();
+                            break;
+                        case "":
+
+                            break;
+                    }
+                }
+                else res.Text = "Не верно произнесенная или неопознанная команда";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex) + 
+                    "\n\n   Программа будет работать не корректно, пока не будет исправлена ошибка!", "Error!");
+            }
+        }
+    }
+}
